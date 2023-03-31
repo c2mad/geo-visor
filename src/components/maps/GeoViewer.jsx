d9300beeeb6d6e0935d1593f650c0b445eac1c85
Oscar Calle <guasaplay@gmail.com>
2023-03-31 09:15:57 -0500

    Refactorizado

--- src/components/maps/GeoViewer.jsx
+++ src/components/maps/GeoViewer.jsx
@@ -1,401 +1,142 @@
-import { MapContainer, GeoJSON } from "react-leaflet";
-import { PluginWrapper } from "./plugins/PluginWrapper";
-import * as ReactDOMServer from "react-dom/server";
 import L from "leaflet";
-import pinMarker from "../../images/pin-marker.png";
-import redMarker from "../../images/red-marker.png";
-import { TableData } from "./TableData";
+import "leaflet-contextmenu";
+import "leaflet-contextmenu/dist/leaflet.contextmenu.css";
+import "leaflet-measure/dist/leaflet-measure.css";
+import "leaflet-measure/dist/leaflet-measure.es";
+import "leaflet-minimap/src/Control.MiniMap";
+import "leaflet-minimap/src/Control.MiniMap.css";
+import "leaflet-ruler";
+import "leaflet-ruler/src/leaflet-ruler.css";
+import "leaflet-switch-basemap/src/L.switchBasemap";
+import "leaflet-switch-basemap/src/L.switchBasemap.css";
+import "leaflet.fullscreen";
+import "leaflet.fullscreen/Control.FullScreen.css";
+import "leaflet.locatecontrol";
+import "leaflet.locatecontrol/dist/L.Control.Locate.min.css";
+import { useEffect } from "react";
 
-const centerPoint = [-2.8573835, -78.9633863];
+const position = [-2.855964, -78.965879];
 
-const parkIcon = new L.Icon({
-  iconUrl: redMarker,
-  iconSize: [40, 40],
-  popupAnchor: [0, -15],
-  shadowAnchor: [13, 28],
-});
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-const pinIcon = new L.Icon({
-  iconUrl: pinMarker,
-  iconSize: [34, 34],
-  popupAnchor: [0, -15],
-  shadowAnchor: [13, 28],
-});
+const osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
-export const GeoViewer = ({
-  infoGeotecnica,
-  expCalicatas,
-  perfilEstra,
-  isoPeriodos,
-  geoLocal,
-  geoAzuay,
-  parrAzuay,
-  key0,
-  key1,
-  key2,
-  key3,
-  key4,
-  key5,
-  key6,
-}) => {
-  return (
-    <MapContainer
-      style={{
-        height: "100%",
-        width: "100%",
-      }}
-      center={centerPoint}
-      zoom={11}
-    >
-      <PluginWrapper key3={key3} key4={key4} key5={key5} />
-      {key0 && infoGeotecnica && (
-        <GeoJSON
-          key={0}
-          data={infoGeotecnica}
-          pointToLayer={(feature, latlng) => {
-            return L.marker(latlng, {
-              icon: parkIcon,
-            });
-          }}
-          style={() => ({
-            color: "#4a83ec",
-            weight: 2,
-            fillColor: "#1a1d62",
-            fillOpacity: 1,
-          })}
-          onEachFeature={({ properties }, layer) => {
-            layer.bindPopup(
-              ReactDOMServer.renderToString(
-                <PopupKey0 properties={properties} />
-              ),
-              {
-                className: "info-geotecnica",
-              }
-            );
-          }}
-        />
-      )}
+export const GeoViewer = () => {
+  useEffect(() => {
+    // create map
+    const map = L.map("map", {
+      fullscreenControl: true,
+      fullscreenControlOptions: {
+        position: "topleft",
+      },
+      contextmenu: true,
+      contextmenuItems: [
+        {
+          text: "Zoom in",
+          callback: () => {},
+        },
+        {
+          text: "Zoom out",
+          callback: () => {},
+        },
+      ],
+      center: position,
+      zoom: 6,
+      layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution,
+        }),
+      ],
+    });
+    L.control.locate().addTo(map);
+    L.control.ruler().addTo(map);
+    L.control
+      .measure({
+        primaryLengthUnit: "kilometers",
+        secondaryLengthUnit: "meters",
+        primaryAreaUnit: "hectares",
+      })
+      .addTo(map);
 
-      {key1 && expCalicatas && (
-        <GeoJSON
-          key={1}
-          data={expCalicatas}
-          pointToLayer={(feature, latlng) => {
-            return L.marker(latlng, {
-              icon: pinIcon,
-            });
-          }}
-          style={() => ({
-            color: "#991b1b",
-            weight: 2,
-            fillColor: "#991b1b",
-            fillOpacity: 1,
-          })}
-          onEachFeature={({ properties }, layer) => {
-            layer.bindPopup(
-              ReactDOMServer.renderToString(
-                <PopupKey1 properties={properties} />
-              )
-            );
-          }}
-        />
-      )}
+    const osm2 = new L.TileLayer(osmUrl, {
+      minZoom: 0,
+      maxZoom: 13,
+      attribution,
+    });
+    new L.Control.MiniMap(osm2).addTo(map);
 
-      {key2 && perfilEstra && (
-        <GeoJSON
-          key={2}
-          data={perfilEstra}
-          style={() => ({
-            color: "#4C3222",
-            weight: 4,
-            fillColor: "#ef4444",
-            fillOpacity: 1,
-          })}
-          onEachFeature={({ properties }, layer) => {
-            layer.bindPopup(
-              ReactDOMServer.renderToString(
-                <PopupKey2 properties={properties} />
-              )
-            );
-          }}
-        />
-      )}
+    new L.basemapsSwitcher(
+      [
+        {
+          layer: L.tileLayer(
+            "https://tile.tracestrack.com/es-name/{z}/{x}/{y}.png?key=53d06df7c98ca08828dc0935570f65cc",
+            {
+              attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }
+          ).addTo(map), //DEFAULT MAP
+          icon: "./images/map.webp",
+          name: "Base",
+        },
+        {
+          layer: L.tileLayer(
+            "https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=E0FAfE58p65fqwIEHhfd",
+            {
+              attribution: "Maptiler - OpenStreetMap, Open Database",
+            }
+          ),
+          icon: "./images/map.webp",
+          name: "Satelital",
+        },
+        {
+          layer: L.tileLayer(
+            "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=E0FAfE58p65fqwIEHhfd",
+            {
+              attribution: "Maptiler - OpenStreetMap, Open Database",
+            }
+          ),
+          icon: "./images/map.webp",
+          name: "Vias",
+        },
+        {
+          layer: L.tileLayer(
+            "https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=E0FAfE58p65fqwIEHhfd",
+            {
+              attribution: "Maptiler - OpenStreetMap, Open Database",
+            }
+          ),
+          icon: "./images/map.webp",
+          name: "Topográfico",
+        },
+      ],
+      { position: "bottomleft" }
+    ).addTo(map);
 
-      {key3 && isoPeriodos && (
-        <GeoJSON
-          key={3}
-          data={isoPeriodos}
-          style={() => ({
-            weight: 3,
-            fillColor: "#84cc16",
-            fillOpacity: 1,
-          })}
-          onEachFeature={({ properties }, layer) => {
-            layer.setStyle({
-              color: properties.color,
-            });
+    L.tileLayer
+      .wms("http://192.168.10.4:8085/geoserver/wms", {
+        layers: "cate:EXPORT-PERFIL,topp:states",
+        format: "image/png",
+        transparent: true,
+        version: "1.1.0",
+        attribution: "myattribution",
+      })
+      .addTo(map);
 
-            layer.bindPopup(
-              ReactDOMServer.renderToString(
-                <PopupKey3 properties={properties} />
-              )
-            );
-          }}
-        />
-      )}
-
-      {key4 && geoLocal && (
-        <GeoJSON
-          key={4}
-          data={geoLocal}
-          style={() => ({
-            color: "#57534e",
-            weight: 1,
-            fillOpacity: 1,
-          })}
-          onEachFeature={({ properties }, layer) => {
-            const { Color = "" } = properties;
-
-            layer.bindPopup(
-              ReactDOMServer.renderToString(
-                <PopupKey4 properties={properties} />
-              )
-            );
-
-            layer.setStyle({
-              fillColor: Color,
-            });
-          }}
-        />
-      )}
-
-      {key5 && geoAzuay && (
-        <GeoJSON
-          key={5}
-          data={geoAzuay}
-          style={() => ({
-            color: "#64748b",
-            weight: 1,
-            fillOpacity: 1,
-          })}
-          onEachFeature={({ properties }, layer) => {
-            const { Color = "" } = properties;
-
-            layer.bindPopup(
-              ReactDOMServer.renderToString(
-                <PopupKey5 properties={properties} />
-              )
-            );
-
-            layer.setStyle({
-              fillColor: Color,
-            });
-          }}
-        />
-      )}
-
-      {key6 && parrAzuay && (
-        <GeoJSON
-          key={6}
-          data={parrAzuay}
-          style={() => ({
-            color: "#f97316",
-            weight: 2,
-            fillColor: "#ca8a04",
-            fillOpacity: 0,
-          })}
-          onEachFeature={({ properties }, layer) => {
-            const { Color = "", DPA_DESPAR, DPA_DESCAN } = properties;
-
-            layer.setStyle({
-              fillColor: Color,
-            });
-
-            layer.bindTooltip(DPA_DESPAR, {
-              permanent: true,
-              direction: "center",
-              className: "tooltip-parroquia",
-            });
-
-            layer.bindPopup(DPA_DESCAN, {
-              className: "popup-parroquia",
-            });
-          }}
-        />
-      )}
-    </MapContainer>
-  );
+    return () => {
+      map.remove();
+    };
+  }, []);
+  return <div id="map" className="h-full w-full"></div>;
 };
-
-const PopupKey0 = ({ properties }) => {
-  const { Fuente, Documento, Xo, Yo, NEC_, SUCS_, Vs30 } = properties;
-
-  return (
-    <div>
-      <h2 className="text-center text-sm font-medium text-slate-700">
-        {Fuente}
-      </h2>
-
-      <div className="mt-3 divide-y rounded-lg border border-gray-300">
-        <PopupItemTable label="Documento" value={Documento} />
-        <PopupItemTable
-          label="Coordenada UTM WGS84 este (latitud)"
-          value={Xo}
-        />
-        <PopupItemTable
-          label="Coordenada UTM WGS84 norte (longitud)"
-          value={Yo}
-        />
-        <PopupItemTable label="NEC_" value={NEC_} />
-        <PopupItemTable label="SUCS_" value={SUCS_} />
-        <PopupItemTable label="Vs30" value={Vs30} />
-      </div>
-      <div className="mt-2 overflow-hidden">
-        <TableData properties={properties} />
-      </div>
-    </div>
-  );
-};
-
-const PopupKey1 = ({ properties }) => {
-  const { Fuente, Documento, Coord_X, Coord_Y, img_origin, img_resize } =
-    properties;
-  return (
-    <div>
-      <h2 className="text-center text-sm font-medium text-slate-700">
-        {Fuente}
-      </h2>
-
-      <div className="mt-3 divide-y rounded-lg border border-gray-300">
-        <PopupItemTable label="Documento" value={Documento} />
-        <PopupItemTable
-          label="Coordenada UTM WGS84 este (latitud)"
-          value={Coord_X}
-        />
-        <PopupItemTable
-          label="Coordenada UTM WGS84 norte (longitud)"
-          value={Coord_Y}
-        />
-      </div>
-
-      <div className="mt-2 flex justify-center">
-        <a href={img_origin} target="_blank" className="h-36" rel="noreferrer">
-          <img
-            src={img_resize}
-            alt={Fuente}
-            className="h-full w-full object-cover"
-          />
-        </a>
-      </div>
-    </div>
-  );
-};
-
-const PopupKey2 = ({ properties }) => {
-  const { Fuente, Documento, img_origin, img_resize } = properties;
-
-  return (
-    <div>
-      <h2 className="text-center text-sm font-medium text-slate-700">
-        {Fuente}
-      </h2>
-
-      <div className="mt-3 divide-y rounded-lg border border-gray-300">
-        <PopupItemTable label="Documento" value={Documento} />
-      </div>
-
-      <div className="mt-2">
-        <a
-          href={img_origin}
-          target="_blank"
-          className="w-full"
-          rel="noreferrer"
-        >
-          <img
-            src={img_resize}
-            alt={Fuente}
-            className="h-full w-full object-cover"
-          />
-        </a>
-      </div>
-    </div>
-  );
-};
-
-const PopupKey3 = ({ properties }) => {
-  const { Fuente, isoperiodo, Documento } = properties;
-
-  return (
-    <div>
-      <h2 className="text-center text-sm font-medium text-slate-700">
-        {Fuente}
-      </h2>
-
-      <div className="mt-3 divide-y rounded-lg border border-gray-300">
-        <PopupItemTable label="Isoperiodo" value={isoperiodo} />
-        <PopupItemTable label="Documento" value={Documento} />
-      </div>
-    </div>
-  );
-};
-
-const PopupKey4 = ({ properties }) => {
-  const {
-    Fuente,
-    Geotecnia,
-    un_geolog,
-    simbolo,
-    Materiales,
-    Comportami,
-    susceptibi,
-  } = properties;
-  return (
-    <div>
-      <h2 className="text-sm font-medium text-slate-700">{Fuente}</h2>
-
-      <div className="mt-3 divide-y rounded-lg border border-gray-300">
-        <PopupItemTable label="Geología" value={Geotecnia} />
-        <PopupItemTable
-          label="Formación geológica"
-          value={`${un_geolog} (${simbolo})`}
-        />
-        <PopupItemTable label="Litología" value={Materiales} />
-        <PopupItemTable label="Comportamiento geológico" value={Comportami} />
-        <PopupItemTable
-          label="Susceptibilidad a deslizamientos"
-          value={susceptibi}
-        />
-      </div>
-    </div>
-  );
-};
-
-const PopupKey5 = ({ properties }) => {
-  const { CATEGORIAS, FORMACION, Fuente, LITOLOGIA, SIMBOLO } = properties;
-
-  return (
-    <div>
-      <h2 className="text-sm font-medium text-slate-700">{Fuente}</h2>
-
-      <div className="mt-3 divide-y rounded-lg border border-gray-300">
-        <PopupItemTable label="Geología" value={CATEGORIAS} />
-        <PopupItemTable
-          label="Formación geológica"
-          value={`${FORMACION} (${SIMBOLO})`}
-        />
-
-        <PopupItemTable label="Litología" value={LITOLOGIA} />
-      </div>
-    </div>
-  );
-};
-
-const PopupItemTable = ({ label, value }) => {
-  return (
-    <div className="p-2">
-      <p className="!my-0 text-sm font-medium text-slate-700">
-        <span className="font-semibold text-slate-600">{label}:</span> {value}
-      </p>
-    </div>
-  );
-};
+{
+  /* <GeoJSON
+        data={data}
+        style={() => ({
+          color: "#4a83ec",
+          weight: 0.5,
+          fillColor: "#1a1d62",
+          fillOpacity: 1,
+        })}
+      /> */
+}
---
+++ src/routers/PublicRoute.jsx
@@ -0,0 +1,5 @@
+import { DefaultLayout } from "layouts/DefaultLayout";
+
+export const PublicRoute = () => {
+  return <DefaultLayout />;
+};
