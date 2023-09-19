924b60ad72de3c1426983ab41ebafc153de55505
yanditv <yankur44@gmail.com>
2023-09-19 15:13:18 -0500

    updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Footer from "components/ui/footer";
 // import imgturismo from "assets/images/mapas-turismo.jpg";
 import "../assets/css/home.css";
 import categoties from "../utils/categories";
+import proyects from "../utils/proyects";
 export const HomePage = () => {
   return (
     <>
@@ -113,6 +114,33 @@ export const HomePage = () => {
             </section>
             <section className="h-32"></section>
           </div>
+        </div>
+        <div className="mx-auto w-full max-w-[1100px] px-3">
+          <h3 className="mb-6 text-center text-4xl font-semibold text-black">
+            Recientemente publicados
+          </h3>
+          {proyects.slice(0, 3).map((proyect) => (
+            <div className="my-5">
+              <a
+                href="#"
+                className="flex flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row"
+              >
+                <img
+                  className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                  src={proyect.imageUrl}
+                  alt=""
+                />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {proyect.name}
+                  </h5>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {proyect.description}
+                  </p>
+                </div>
+              </a>
+            </div>
+          ))}
         </div>
         <div>
           <Footer />
---
+++ src/utils/proyects.jsx
@@ -0,0 +1,196 @@
+const proyects = [
+  {
+    id: 1,
+    title: "Estructura Vial de la Ciudad de Cuenca",
+    imageUrl:
+      "https://img.freepik.com/vector-gratis/concepto-cartografia-mapa-mundial-vintage_52683-26377.jpg?w=996&t=st=1694624780~exp=1694625380~hmac=c88ea11eeddf234ce3e67a26054e6ffd998268aa8e8731a310d34cef3166d448",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "DatAchademics",
+    layeritem: "Leer más",
+    category: 6,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Suelo",
+    imageUrl:
+      "https://img.freepik.com/vector-gratis/dibujo-tema-vintage-mapa-mundial_52683-26577.jpg?w=996&t=st=1694466163~exp=1694466763~hmac=772fae750ab1eda669bf83604d7bb8d8ce7de80a4f228dbd9a9e72f2d9a71c71",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "C2MAD",
+    layeritem: "Leer más",
+    category: 2,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Turístico",
+    imageUrl:
+      "https://img.freepik.com/vector-gratis/mapa-mundo-tecnologia-global-o-red-conexion-social-nodos-enlaces-ilustracion-vectorial_1284-1968.jpg?w=996&t=st=1694467334~exp=1694467934~hmac=52dde44b546abe97fe3fe6c7971ec29c83f514494102398ec6b30b30249731ae",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "DatAchademics",
+    layeritem: "Leer más",
+    category: 5,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Infraestructura",
+    imageUrl:
+      "https://us.123rf.com/450wm/bsd555/bsd5552102/bsd555210201744/163486742-icono-de-color-rgb-de-ingenier%C3%ADa-de-infraestructura-municipal-construcci%C3%B3n-urbana-negocio.jpg",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "RIOUC",
+    layeritem: "Leer más",
+    category: 1,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 5,
+    title: "Infraestructura",
+    imageUrl:
+      "https://us.123rf.com/450wm/bsd555/bsd5552102/bsd555210201744/163486742-icono-de-color-rgb-de-ingenier%C3%ADa-de-infraestructura-municipal-construcci%C3%B3n-urbana-negocio.jpg",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "RIOUC",
+    layeritem: "Leer más",
+    category: 1,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 6,
+    title: "Turístico",
+    imageUrl:
+      "https://static.vecteezy.com/system/resources/previews/008/201/844/non_2x/travel-agency-tourism-app-and-trips-logo-outdoor-adventure-emblems-emblem-design-on-white-background-vector.jpg",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "DatAchademics",
+    layeritem: "Leer más",
+    category: 1,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 7,
+    title: "Suelo",
+    imageUrl:
+      "https://i.pinimg.com/originals/e2/54/f1/e254f165f0c63e235aa9ecb49ea489a2.jpg",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "DatAchademics",
+    layeritem: "Leer más",
+    category: 1,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 8,
+    title: "Vial",
+    imageUrl:
+      "https://us.123rf.com/450wm/bsd555/bsd5552003/bsd555200304114/143535533-conjunto-de-iconos-de-color-rgb-de-obras-viales-cono-de-tr%C3%A1fico-barrera-vial-carro-de-rodillos.jpg?ver=6",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "C2MAD",
+    layeritem: "Leer más",
+    category: 1,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 9,
+    title: "Vial",
+    imageUrl:
+      "https://us.123rf.com/450wm/bsd555/bsd5552003/bsd555200304114/143535533-conjunto-de-iconos-de-color-rgb-de-obras-viales-cono-de-tr%C3%A1fico-barrera-vial-carro-de-rodillos.jpg?ver=6",
+    publication: "12 de Agosto 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at.",
+    fuente: "DatAchademics",
+    layeritem: "Leer más",
+    category: 1,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 10,
+    title: "Suelo",
+    imageUrl:
+      "https://i.pinimg.com/originals/e2/54/f1/e254f165f0c63e235aa9ecb49ea489a2.jpg",
+    publication: "12 de Agosto de 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at",
+    fuente: "DatAchademics",
+    layeritem: "Leer más",
+    category: 1,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 11,
+    title: "Turístico",
+    imageUrl:
+      "https://static.vecteezy.com/system/resources/previews/008/201/844/non_2x/travel-agency-tourism-app-and-trips-logo-outdoor-adventure-emblems-emblem-design-on-white-background-vector.jpg",
+    publication: "12 de Agosto de 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at",
+    fuente: "C2MAD",
+    layeritem: "Leer más",
+    category: 6,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  {
+    id: 12,
+    title: "Infraestructura",
+    imageUrl:
+      "https://us.123rf.com/450wm/bsd555/bsd5552102/bsd555210201744/163486742-icono-de-color-rgb-de-ingenier%C3%ADa-de-infraestructura-municipal-construcci%C3%B3n-urbana-negocio.jpg",
+    publication: "12 de Agosto de 2023",
+    description:
+      "It is difficult to believe that we have become so used to having instant access to information at",
+    fuente: "RIOUC",
+    layeritem: "Leer más",
+    category: 3,
+    autores: [
+      { id: 1, name: "Sandra Cobos" },
+      { id: 2, name: "Setefano Torrachi" },
+    ],
+  },
+  // ... (tus elementos)
+];
+export default proyects;
