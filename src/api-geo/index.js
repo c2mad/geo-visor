edaeeef7c2528c22f44dfd8e3d0e7cad20858113
yanditv <yankur44@gmail.com>
2024-01-29 09:52:09 -0500

    updated

--- src/api-geo/index.js
+++ src/api-geo/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://192.168.10.4:8085";
+//const baseURL = "http://192.168.10.4:8085";
+const baseURL = "https://13b0-190-15-134-178.ngrok-free.app";
 
 const geoApi = axios.create({ baseURL });
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,10 @@ module.exports = function(app) {
     app.use(
       '/geoserver/wfs',
       createProxyMiddleware({
-        target: 'http://192.168.10.4:8085', // Reemplaza con tu URL de API
+        //target: 'http://192.168.10.4:8085', // Reemplaza con tu URL de API
+        target: 'https://ec1c-190-15-134-178.ngrok-free.app', // Reemplaza con tu URL de API
         changeOrigin: true,
-        secure: false
+        secure: false,
       })
     );
   }
