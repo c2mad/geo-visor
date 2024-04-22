07dbc613c95094ed8cd78c82570cf725d8c18c68
yanditv <yankur44@gmail.com>
2024-04-22 11:52:35 -0500

    updated

--- src/utils/host_api.js
+++ src/utils/host_api.js
@@ -1,3 +1,3 @@
-const host_api = " https://dff7-190-15-134-178.ngrok-free.app";
+const host_api = "https://f7ed-190-15-134-178.ngrok-free.app";
 //const host_api = "http://192.168.10.4:8085";
 export default host_api;
